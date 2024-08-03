0cbc611fa70a40d5f90bfb8e2ae6529d733187e8
sahil khan <112230611+mrsk092@users.noreply.github.com>
2024-08-03 17:05:16 +0530

    Updated app.js
    1. functionality to detect empty task and task only contains spaces , this type of tasks are not allowed to be added on todo.
    
    2. add task using enter button from key board

--- app.js
+++ app.js
@@ -5,57 +5,17 @@ let ul = document.querySelector("ul");
 
 btn.addEventListener("click",function(){
      let item =document.createElement("li");
-     let taskText = document.createElement("span");
-
-     taskText.innerText=inp.value;
-
-  
-
-     item.appendChild(taskText);
+     item.innerText=inp.value;
      
      ul.appendChild(item);
-
-     let btnContener = document.createElement("div");
-
-     btnContener.classList.add("btn-contener");
-
-
-     let checkBtn = document.createElement("button");
-     checkBtn.innerText="✓";
-
-     checkBtn.classList.add("check-btn");
-     
-  
-
      let dltBtn = document.createElement("button");
      dltBtn.innerText="X";
-
-   
-
-     dltBtn.classList.add("delete-btn");
-
-      btnContener.appendChild(checkBtn);
-      btnContener.appendChild(dltBtn);
-
-      item.appendChild(btnContener);
+     dltBtn.addEventListener("click",function(){
+        let parent = this.parentElement;
+        parent.remove();
+     });
+     
+     item.append(" ",dltBtn);
      inp.value=""
-
-});
-
-//add the event to the parent will aslo triggerd from the chiled
-
-ul.addEventListener("click",function(event){
-   let targetItem = event.target.parentElement.parentElement; //it targets the li which is parant of btn div
-  
-    if(event.target.innerText=="X"){
-  
-      targetItem.remove();
-    }
-    else if(event.target.innerText=="✓"){
-        let taskText = targetItem.firstElementChild; //the parant is li and its firs child is span
-
-        taskText.classList.add("cross");
-
-    }
 
 });
